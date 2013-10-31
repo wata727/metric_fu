db922edfcb69773a8c37a76fe18d681cc1296df9
Benjamin Fleischer <github@benjaminfleischer.com>
2013-10-31 15:26:24 -0500

    Remove metric_fu_requires

--- spec/metric_fu/gem_version_spec.rb
+++ spec/metric_fu/gem_version_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
 MetricFu.lib_require { 'gem_version' }
-require 'metric_fu_requires'
 
 describe MetricFu::GemVersion do
 
@@ -10,10 +9,6 @@ describe MetricFu::GemVersion do
     MetricFu::Metric.metrics.reject{|metric| metric.name == :hotspots || metric.name == :stats}.map(&:name).map(&:to_s).each do |metric_name|
       expect(gem_deps).to include(metric_name)
     end
-  end
-
-  it 'behaves like metric_fu_requires' do
-    expect(MetricFu::GemVersion.for('saikuro')).to eq(MetricFu::MetricVersion.saikuro)
   end
 
 end
