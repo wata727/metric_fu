9a548a1017c672cf059882760eb17a14eb2d22ef
Benjamin Fleischer <github@benjaminfleischer.com>
2013-11-22 13:01:36 -0600

    Metrics require generator and grapher on activation

--- lib/metric_fu/metric.rb
+++ lib/metric_fu/metric.rb
@@ -1,5 +1,6 @@
 require 'set'
 MetricFu.lib_require { 'gem_run' }
+MetricFu.metrics_require { 'generator' }
 # Encapsulates the configuration options for each metric
 module MetricFu
   class Metric
@@ -18,6 +19,7 @@ module MetricFu
 
     # TODO: Confirm this catches load errors from requires in subclasses, such as for flog
     def activate
+      MetricFu.metrics_require { default_metric_library_paths }
       @libraries.each {|library| require(library) }
       self.activated = true
     rescue LoadError => e
@@ -120,5 +122,13 @@ module MetricFu
       @libraries << file.strip
     end
 
+    def default_metric_library_paths
+      paths = []
+      paths << generator_path = "#{name}/#{name}"
+      if has_graph?
+          paths << grapher_path   = "#{name}/#{name}_grapher"
+      end
+      paths
+    end
   end
 end
