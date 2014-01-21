28235f16ceb8b6374ef18ccd3cf464e3dc723053
Benjamin Fleischer <github@benjaminfleischer.com>
2014-01-21 10:55:16 -0600

    Fix Reek spec from #196

--- spec/metric_fu/configuration_spec.rb
+++ spec/metric_fu/configuration_spec.rb
@@ -177,7 +177,7 @@ describe MetricFu::Configuration do
       it 'should set @reek to {:dirs_to_reek => @code_dirs}' do
         load_metric 'reek'
         expect(MetricFu::Metric.get_metric(:reek).run_options).to eq(
-                {:config_file_pattern=>'config/*.reek', :dirs_to_reek => ['lib']}
+                {:config_file_pattern=>nil, :dirs_to_reek => ['lib']}
         )
       end
 
