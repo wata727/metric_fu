d0d662bc4720d36145d0b714aa5be8a7a3acfa54
Benjamin Fleischer <github@benjaminfleischer.com>
2014-03-09 16:24:31 -0500

    Run usage test specs under JRuby

--- spec/usage_test_spec.rb
+++ spec/usage_test_spec.rb
@@ -40,21 +40,30 @@ describe 'usage test' do
     specify 'fails when the command runs with an error' do
       code = "sandwhich_fu ruby"
       test_result = SnippetRunner.new(code, 'sh').run_code
-      expect(test_result.captured_output).to match('Errno::ENOENT')
+      expect(test_result.captured_output).to match(failed_command_error)
       expect(test_result.success).to eq(false)
     end
     specify 'succeeds when the code exits with a zero exit status' do
-      code = "which ruby; exit 0"
+      code = "sh #{fixtures_path.join('exit0.sh').to_path}"
       test_result = SnippetRunner.new(code, 'sh').run_code
       expect(test_result.captured_output).to match("exit status 0")
       expect(test_result.success).to eq(true)
     end
     specify 'fails when the code exits with a non-zero exit status' do
-      code = "which ruby; exit 1"
+      code = "sh #{fixtures_path.join('exit1.sh').to_path}"
       test_result = SnippetRunner.new(code, 'sh').run_code
       expect(test_result.captured_output).to match("exit status 1")
       expect(test_result.success).to eq(false)
     end
 
+  end
+
+  def failed_command_error
+    defined?(JRUBY_VERSION) ? 'IOError' : 'Errno::ENOENT'
+  end
+
+  def fixtures_path
+    fixtures_dir = File.join(File.expand_path('..', File.dirname(__FILE__)), 'spec', 'fixtures')
+    Pathname(fixtures_dir)
   end
 end
