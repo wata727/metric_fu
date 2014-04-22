6d7a72dfd66c4ad1f608844bd936b1873b04bdda
Benjamin Fleischer <github@benjaminfleischer.com>
2014-04-22 17:13:07 -0500

    Test and document MetricFu::Templates::Report

--- lib/metric_fu/templates/report.rb
+++ lib/metric_fu/templates/report.rb
@@ -1,17 +1,23 @@
 MetricFu.lib_require { 'formatter/syntax' }
 
+# Creates an HTML document for a given analyzed file,
+# with scored metrics annotating the relevant line.
 module MetricFu
   module Templates
     class Report < MetricFu::Template
 
+      # @param file [String] the analyzed file to annotate
+      # @param lines [Hash] of line number [String] keyed to an list [[Array] of metrics for that line. Each metric in the list is a hash containing the keys :type => metric_name, :descrption => metric_score
+      # @example file and lines
+      #   file: "lib/metric_fu/gem_version.rb
+      #   lines: {"30"=>[{:type=>:flog, :description=>"Score of 22.43"}], "42"=>[{:type=>:flog, :description=>"Score of 8.64"}]}
       def initialize(file, lines)
         @file = file
         @lines = lines
         @data = File.open(file, 'rb') {|f| f.readlines }
       end
 
-      def render(metrics)
-        @metrics = metrics
+      def render
         erbify('report')
       end
 
