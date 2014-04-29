5e6c33728d99688d81862cf74da5f9bacef7c8d5
Benjamin Fleischer <github@benjaminfleischer.com>
2014-04-28 21:16:26 -0500

    Skip RCov tests that depend on SimpleCov not running
    Apparently 'pending' now runs the spec

--- spec/metric_fu/metrics/rcov/simplecov_formatter_spec.rb
+++ spec/metric_fu/metrics/rcov/simplecov_formatter_spec.rb
@@ -23,27 +23,29 @@ describe SimpleCov::Formatter::MetricFu do
     expect(File.exists?( @rcov_file )).to be_truthy
   end
 
-  it "test_create_content" do
-    pending "can't run with simplecov" if SimpleCov.running
-    content = SimpleCov::Formatter::MetricFu::FormatLikeRCov.new(@result).format
-    test = "\="*80
+  if SimpleCov.running
+    MetricFu.logger.info "Skipping specs while SimpleCov is running"
+  else
+    it "test_create_content" do
+      content = SimpleCov::Formatter::MetricFu::FormatLikeRCov.new(@result).format
+      test = "\="*80
 
-    expect(content).to match(/#{test}/)
-    expect(content).to match(/!!     value \* value/)
-  end
+      expect(content).to match(/#{test}/)
+      expect(content).to match(/!!     value \* value/)
+    end
 
-  it 'calculates the same coverage from an RCov report as from SimpleCov' do
-    pending "can't run with simplecov" if SimpleCov.running
-    SimpleCov.start # start coverage
-    require 'fixtures/coverage-153'
-    result = SimpleCov.result # end coverage
-    source_file = result.source_files.first
+    it 'calculates the same coverage from an RCov report as from SimpleCov' do
+      SimpleCov.start # start coverage
+      require 'fixtures/coverage-153'
+      result = SimpleCov.result # end coverage
+      source_file = result.source_files.first
 
-    simplecov_coverage =  source_file.lines.partition{|line| line.missed? }.map(&:count)
+      simplecov_coverage =  source_file.lines.partition{|line| line.missed? }.map(&:count)
 
-    rcov_text = SimpleCov::Formatter::MetricFu::FormatLikeRCov.new(result).format
-    analyzed_rcov_coverage = MetricFu::RCovFormatCoverage.new(rcov_text).to_h.first[1][:lines].partition{|line| !line[:was_run]}.map(&:count)
+      rcov_text = SimpleCov::Formatter::MetricFu::FormatLikeRCov.new(result).format
+      analyzed_rcov_coverage = MetricFu::RCovFormatCoverage.new(rcov_text).to_h.first[1][:lines].partition{|line| !line[:was_run]}.map(&:count)
 
-    expect(analyzed_rcov_coverage).to eq(simplecov_coverage)
+      expect(analyzed_rcov_coverage).to eq(simplecov_coverage)
+    end
   end
 end
